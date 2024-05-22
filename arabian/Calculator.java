43707cbf80324b014456cc529fb4898f6d72a28f
8962881 <89628816786@mail.ru>
2024-05-23 00:48:11 +0300

    Update Calculator.java

--- arabian/Calculator.java
+++ arabian/Calculator.java
@@ -1,36 +1,78 @@
-package arabian;
+package kalk;
 import java.util.Scanner;
-public class Calculator {
+import java.util.concurrent.LinkedTransferQueue;
+
+public class cher {
     public static void main(String[] args) {
+
         Converter converter = new Converter();
         String[] actions = {"+", "-", "/", "*"};
         String[] regexActions = {"\\+", "-", "/", "\\*"};
         Scanner scn = new Scanner(System.in);
         System.out.print("Введите выражение: ");
         String exp = scn.nextLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         int actionIndex=-1;
         for (int i = 0; i < actions.length; i++) {
             if(exp.contains(actions[i])){
                 actionIndex = i;
                 break;
             }
+
         }
+
         if(actionIndex==-1){
             System.out.println("Некорректное выражение");
             return;
         }
+
+
+
+
+
+
+
+
+
+
+
         String[] data = exp.split(regexActions[actionIndex]);
 
         if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
             int a,b;
-            boolean isRim = converter.isRoman(data[0]);
-            if(isRim){
-                a = converter.rimToInt(data[0]);
-                b = converter.rimToInt(data[1]);
-            }else{
+
+            boolean isRoman = converter.isRoman(data[0]);
+            boolean lenEx=false;
+            if(isRoman){
+
+                a = converter.romanToInt(data[0]);
+                b = converter.romanToInt(data[1]);
+
+            }
+
+
+            else{
+
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
             }
+
+
             int result;
             switch (actions[actionIndex]){
                 case "+":
@@ -46,17 +88,20 @@ public class Calculator {
                     result = a/b;
                     break;
             }
-            if(isRim){
 
-                System.out.println(converter.intToRim(result));
+            if(isRoman){
+
+                System.out.println(converter.intToRoman(result));
             }
             else{
 
                 System.out.println(result);
             }
-        }
-        else{
+        }else{
             System.out.println("Числа должны быть в одном формате");
         }
+
+
     }
 }
+        
